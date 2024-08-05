e9846fdefffa56fd77ecdc102a9bb3a49e50b05a
N1186828 <alex.burrows2023@my.ntu.ac.uk>
2024-08-05 16:16:09 +0100

    Sorted out obstacles, so you HAVE to jump over them or duck under them now. I have also done some general code maintenance as well such as the removal of random comments and lines of code. If I come back to this project there are a few things which can be improved/amended: - Speed of the game, the current speed is fine but when you try and speed the game up it messes up the spawning of obstacles - Access to the external data file, does not work as of now.

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -4,29 +4,25 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
-#include<fstream>
+#include <fstream>
 #include "Player.h"
 #include "Obstacle.h"
 
-using namespace std; 
+using namespace std;
 
-int score = 0; 
-double Player_xvel = 0; 
+int score = 0;
+double Player_xvel = 0;
 double Player_yvel = 0;
 int player_x = 100;
 int player_y = 550;
-bool played_game = false; 
-int Screen_Height = 750; 
+bool played_game = false;
+int Screen_Height = 750;
 int Screen_Width = 1250;
 //Game Related
 bool obstacle1_active = false;
 bool obstacle2_active = false;
-bool obstacle1_clear = true; 
+bool obstacle1_clear = true;
 bool obstacle2_clear = true;
-
-//Obstacle Variables 
-
-
 
 void main_menu();
 void PlayGame(SDL_Renderer* render, SDL_Window* window);
@@ -54,7 +50,7 @@ void main_menu()
 	window = SDL_CreateWindow("Dino Runner", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Screen_Width, Screen_Height, SDL_WINDOW_SHOWN);
 
 	//Creation of Renderer 
-	SDL_Renderer* render = NULL; 
+	SDL_Renderer* render = NULL;
 	render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	//Draw Background Colour
@@ -62,11 +58,11 @@ void main_menu()
 	SDL_RenderClear(render);
 
 	//Creation of Start Button
-	SDL_Rect Start; 
-	Start.x = 550 ; 
-	Start.y = 600 ; 
-	Start.w = 200 ; 
-	Start.h = 100 ;
+	SDL_Rect Start;
+	Start.x = 550;
+	Start.y = 600;
+	Start.w = 200;
+	Start.h = 100;
 
 	//Putting the Start Button on Screen 
 	SDL_SetRenderDrawColor(render, 0, 255, 0, 255);
@@ -82,7 +78,7 @@ void main_menu()
 	text(Black, render, 400, 300, "HIGH SCORE:");
 	//Need to access external text file and access the high score and then display it next to the above text
 	string high_score = highscore();
-	stringstream hs; 
+	stringstream hs;
 	hs << high_score;
 	text(Black, render, 500, 300, hs.str().c_str());
 
@@ -100,8 +96,8 @@ void main_menu()
 	SDL_RenderClear(render);
 
 	//Main Game Loop 
-	SDL_Event event; 
-	bool active = true; 
+	SDL_Event event;
+	bool active = true;
 	while (active)
 	{
 		while (SDL_PollEvent(&event))
@@ -109,14 +105,14 @@ void main_menu()
 			//Allows the game to be exited 
 			if (event.type == SDL_QUIT)
 			{
-				active = false; 
+				active = false;
 				break;
 			}
 
 			else if (event.type == SDL_MOUSEBUTTONDOWN)
 			{
 				SDL_Point MousePosition = { 0,0 };
-				SDL_GetMouseState(& MousePosition.x, & MousePosition.y);
+				SDL_GetMouseState(&MousePosition.x, &MousePosition.y);
 
 				if (SDL_PointInRect(&MousePosition, &Start))
 				{
@@ -138,32 +134,34 @@ void main_menu()
 void PlayGame(SDL_Renderer* render, SDL_Window* window)
 {
 	int obstacle_count = 0;
-	score = 0; 
+	score = 0;
 	double Player_xvel = 0;
 	double Player_yvel = 0;
 	int player_x = 100;
 	int player_y = 550;
+	//Obstacles being active/inactive
 	bool obstacle1_active = false;
 	bool obstacle2_active = false;
 	bool obstacle3_active = false;
 	bool obstacle4_active = false;
+	//Obstacles being on/off the screen. 
 	bool obstacle1_clear = true;
 	bool obstacle2_clear = true;
 	bool obstacle3_clear = true;
 	bool obstacle4_clear = true;
 	//Limit Jump Height 
-	int limitjump = 475 ; 
+	int limitjump = 475;
 	bool jump = false;
 	//Limit Duck Height
-	int limitduck = 625 ;
+	int limitduck = 625;
 	bool duck = false;
 
 	//Creation of Floor Rectangle/Surface (Positioned on screen)
-	SDL_Rect Floor; 
-	Floor.x = 0 ; 
-	Floor.y = 600 ; 
-	Floor.w = 1250 ; 
-	Floor.h = 200 ;
+	SDL_Rect Floor;
+	Floor.x = 0;
+	Floor.y = 600;
+	Floor.w = 1250;
+	Floor.h = 200;
 	//Creation of Second Floor Surface (Positioned off screen, ready to move on screen)
 	SDL_Rect Floor2;
 	Floor2.x = 1249;
@@ -171,15 +169,15 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 	Floor2.w = 1250;
 	Floor2.h = 200;
 	//Player
-	SDL_Rect Player; 
-	Player.x = player_x; 
-	Player.y = player_y; 
-	Player.w = 50; 
+	SDL_Rect Player;
+	Player.x = player_x;
+	Player.y = player_y;
+	Player.w = 50;
 	Player.h = 50;
 	//Obstacle 1
 	int type = 1; //Need to improve this, so a random value between 1 and 2 is called
-	obstacle ground; 
-	SDL_Rect obs; 
+	obstacle ground;
+	SDL_Rect obs;
 	obs.x = 1800;
 	obs.y = ground.obstacle_y(type);
 	obs.w = ground.obstacle_width(type);
@@ -187,41 +185,41 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 
 	//Obstacle 2
 	type = 2;
-	obstacle air; 
-	SDL_Rect obs2; 
+	obstacle air;
+	SDL_Rect obs2;
 	obs2.x = obs.x + 600;
 	obs2.y = air.obstacle_y(type);
 	obs2.w = air.obstacle_width(type);
 	obs2.h = air.obstacle_height(type);
 
 	//Obstacle 3
-	type = 1; 
-	obstacle ground1; 
-	SDL_Rect obs3; 
-	obs3.x = obs2.x + 600; 
+	type = 1;
+	obstacle ground1;
+	SDL_Rect obs3;
+	obs3.x = obs2.x + 600;
 	obs3.y = ground1.obstacle_y(type);
 	obs3.w = ground1.obstacle_width(type);
 	obs3.h = ground1.obstacle_height(type);
 
 	//Obstacle 4 
-	type = 2; 
-	obstacle air1; 
-	SDL_Rect obs4; 
-	obs4.x = obs3.x + 600; 
+	type = 2;
+	obstacle air1;
+	SDL_Rect obs4;
+	obs4.x = obs3.x + 600;
 	obs4.y = air1.obstacle_y(type);
 	obs4.w = air1.obstacle_width(type);
 	obs4.h = air1.obstacle_height(type);
 
-	int last_x = 2800; 
+	int last_x = 2800;
 
 	SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
-	SDL_RenderFillRect(render,&Floor);
+	SDL_RenderFillRect(render, &Floor);
 	SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
 	SDL_RenderFillRect(render, &Floor2);
 	SDL_SetRenderDrawColor(render, 255, 255, 0, 255);
 	SDL_RenderFillRect(render, &Player);
 	SDL_RenderPresent(render);
-	
+
 	SDL_Color Black{ 0,0,0 };
 	text(Black, render, 0, 0, "Score:");
 	stringstream updatedscore;
@@ -240,20 +238,12 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 		if (Player.x > Screen_Width - 650)
 		{
 			Player.x -= Player_xvel; //Player Stops Moving but the ground doesnt, so it gives the impression that the character is moving
-			Floor.x -= Player_xvel; 
+			Floor.x -= Player_xvel;
 			Floor2.x -= Player_xvel;
 			cout << "Point Reached";
 		}
 
-		//if ((Player.y > limitjump) && (jump == true)) 
-		//{
-		//	Player.y = limitjump;
-		//}
-		//if ((Player.y < limitduck) && (duck == true)) 
-		//{
-		//	Player.y = limitduck;
-		//}
-		//Move the floor rect which isnt on screen to the left of the screen 
+		//Allowing for the floor to be endless, as when one floor ends it is brought to the other side. 
 		if (Floor.x < -1249)
 		{
 			Floor.x = 1249;
@@ -271,15 +261,14 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				//Start Spawning Ground Obstacles 
 				obstacle1_active = true;
 				obstacle_count += 1;
-				obstacle1_clear = false; 
+				obstacle1_clear = false;
 				type = 1;
 				SDL_Rect obs;
-				obs.x = ground.obstacle_x(last_x); 
+				obs.x = ground.obstacle_x(last_x);
 				obs.y = ground.obstacle_y(type);
 				obs.w = ground.obstacle_width(type);
 				obs.h = ground.obstacle_height(type);
 				//last_x = obs.x;
-				cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" << obs.x;
 			}
 		}
 
@@ -290,7 +279,7 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				//Start Spawning Flying Obstacles 
 				obstacle2_active = true;
 				obstacle_count += 1;
-				obstacle2_clear = false; 
+				obstacle2_clear = false;
 				type = 2;
 				SDL_Rect obs2;
 				obs2.x = air.obstacle_x(last_x);
@@ -298,7 +287,6 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				obs2.w = air.obstacle_width(type);
 				obs2.h = air.obstacle_height(type);
 				//last_x = obs2.x;
-				cout << "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB" << obs2.x;
 			}
 		}
 
@@ -316,7 +304,6 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				obs3.y = ground1.obstacle_y(type);
 				obs3.w = ground1.obstacle_width(type);
 				obs3.h = ground1.obstacle_height(type);
-				cout << "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE" << obs3.x;
 			}
 		}
 
@@ -334,7 +321,6 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				obs4.y = air1.obstacle_y(type);
 				obs4.w = air1.obstacle_width(type);
 				obs4.h = air1.obstacle_height(type);
-				cout << "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" << obs4.x;
 			}
 		}
 
@@ -344,12 +330,11 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 		}
 		else if (obs.x = -1) //If the rect which is obs has moved off screen then. 
 		{
-			obstacle1_clear = true; 
+			obstacle1_clear = true;
 			obstacle_count -= 1;
-			obstacle1_active = false; 
+			obstacle1_active = false;
 			obs.x = ground.obstacle_x(last_x);
 			last_x = last_x + 150;
-			//cout << "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
 		}
 		if (obs2.x > -1)
 		{
@@ -362,7 +347,6 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 			obstacle2_active = false;
 			obs2.x = air.obstacle_x(last_x);
 			last_x = last_x + 150;
-			//cout << "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD";
 		}
 
 		if (obs3.x > -1)
@@ -376,7 +360,6 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 			obstacle3_active = false;
 			obs3.x = ground1.obstacle_x(last_x);
 			last_x = last_x + 150;
-			//cout << "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ";
 		}
 
 		if (obs4.x > -1)
@@ -390,23 +373,23 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 			obstacle4_active = false;
 			obs4.x = air1.obstacle_x(last_x);
 			last_x = last_x + 150;
-			//cout << "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
 		}
 
 		//Making sure that speed is limited to 1 (most likely to change)
 		if (Player_xvel >= 1) //Basic but works, may change this so it uses time
 		{
-			Player_xvel == 1; //Keep x velocity at 1 
+			Player_xvel == 1; //Keep x velocity at 1
+			//Below Code of Changing speed does work, but need to change the == to =. As it causes a problem with the obstacles.
 			if (score > 700)
 			{
-				Player_xvel == 2; 
+				Player_xvel == 2;
 			}
 			if (score > 1400)
 			{
-				Player_xvel == 3; 
+				Player_xvel == 3;
 			}
-			int count = 1000; 
-			for (int i = 0; i < count; i++) 
+			int count = 1000;
+			for (int i = 0; i < count; i++)
 			{
 				cout << "i num" << i << endl;//Keep this, makes the movement slow...
 			}
@@ -428,30 +411,26 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 			{
 				SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 				SDL_RenderFillRect(render, &obs);
-				cout << "111111111111111111111111111111111111111111111111111111" << endl;
 			}
 			if (obstacle2_active == true)
 			{
 				SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 				SDL_RenderFillRect(render, &obs2);
-				cout << "222222222222222222222222222222222222222222222222222222" << endl;
 			}
 			if (obstacle3_active == true)
 			{
 				SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 				SDL_RenderFillRect(render, &obs3);
-				cout << "555555555555555555555555555555555555555555555555555555" << endl;
 			}
 			if (obstacle4_active == true)
 			{
 				SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 				SDL_RenderFillRect(render, &obs4);
-				cout << "666666666666666666666666666666666666666666666666666666" << endl;
 			}
 			SDL_RenderPresent(render);
 			text(Black, render, 0, 0, "Score:");
-			stringstream updatedscore; 
-			updatedscore << score; 
+			stringstream updatedscore;
+			updatedscore << score;
 			text(Black, render, 100, 0, updatedscore.str().c_str());
 			SDL_RenderPresent(render);
 			SDL_RenderClear(render);
@@ -465,7 +444,7 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 		if ((collision1 == true) || (collision2 == true) || (collision3 == true) || (collision4 == true))
 		{
 			//cout << "GAME OVER" << endl; 
-			played_game = true; 
+			played_game = true;
 			main_menu();
 			//break;
 			SDL_DestroyWindow(window);
@@ -487,16 +466,13 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 			//Moving up and down (for the time being), this will become jumping and ducking in the future
 			else if (event.type == SDL_KEYDOWN)
 			{
-				
+
 				switch (event.key.keysym.sym)
 				{
 				case SDLK_UP:
-					//cout << "up/jump" << endl; 
+					//up/jump action 
 					Player_yvel -= 100;
-					//cout << Player_yvel << endl;
-					//cout << Player.y;
-					//jump = true;
-					if (Player.y < limitjump)
+					if (Player.y < limitjump) //Limits the players jump height to the value specified in the variable of limitjump
 					{
 						Player_yvel = 0;
 						jump = true;
@@ -505,12 +481,11 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 					break;
 
 				case SDLK_DOWN:
-					//cout << "down/duck" << endl;
+					//down/duck action
 					Player_yvel += 100;
-					//cout << Player.y;
-					if (Player.y > limitduck)
+					if (Player.y > limitduck) //Same as above but for ducking to thr value specified in limitduck
 					{
-						Player_yvel = 0; 
+						Player_yvel = 0;
 						duck = true;
 					}
 					break;
@@ -533,25 +508,21 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				{
 					SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 					SDL_RenderFillRect(render, &obs);
-					cout << "333333333333333333333333333333333333333333333333333333" << endl;
 				}
 				if (obstacle2_active == true)
 				{
 					SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 					SDL_RenderFillRect(render, &obs2);
-					cout << "44444444444444444444444444444444444444444444444444444" << endl;
 				}
 				if (obstacle3_active == true)
 				{
 					SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 					SDL_RenderFillRect(render, &obs3);
-					cout << "77777777777777777777777777777777777777777777777777777" << endl;
 				}
 				if (obstacle4_active == true)
 				{
 					SDL_SetRenderDrawColor(render, 111, 255, 111, 255);
 					SDL_RenderFillRect(render, &obs4);
-					cout << "88888888888888888888888888888888888888888888888888888" << endl;
 				}
 				text(Black, render, 0, 0, "Score:");
 				stringstream updatedscore;
@@ -559,7 +530,7 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				text(Black, render, 100, 0, updatedscore.str().c_str());
 				SDL_RenderPresent(render);
 				SDL_RenderClear(render);
-				
+
 			}
 
 			else if (event.type == SDL_KEYUP)
@@ -567,14 +538,12 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 				switch (event.key.keysym.sym)
 				{
 				case SDLK_UP:
-					//cout << "up/jump" << endl; 
+					//up/jump" action; 
 					if (jump == false)
 					{
 						Player_yvel += 100;
 					}
-					//cout << Player_yvel << endl; 
-					//cout << "PLayer Y" << Player.y << endl;
-					Player.y = 550; //Brings Player Back Down
+					Player.y = 550; //Brings Player Back Down to the floor
 					jump = false;
 					break;
 
@@ -584,13 +553,12 @@ void PlayGame(SDL_Renderer* render, SDL_Window* window)
 					{
 						Player_yvel -= 100;
 					}
-					//cout << Player.y << endl;
-					Player.y = 550; //Brings Player Back Down 
-					duck = false; 
+					Player.y = 550; //Brings Player Back Down to the floor
+					duck = false;
 					break;
 				}
 			}
-			if (Player.x > Screen_Width - 650)//
+			if (Player.x > Screen_Width - 650)
 			{
 				Player.x = Screen_Width - 650;
 			}
@@ -613,7 +581,7 @@ void text(SDL_Color Colour, SDL_Renderer* text_render, int x, int y, string text
 	TTF_Font* font = TTF_OpenFont("../Project1/Resources/Arial/arial.ttf", 25);
 	//Location of Font
 	SDL_Rect fontrect = { x,y };
-	stringstream text_creation; 
+	stringstream text_creation;
 	text_creation << text;
 	SDL_Surface* surface = TTF_RenderText_Blended(font, text_creation.str().c_str(), Colour);
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(text_render, surface);
@@ -624,21 +592,21 @@ void text(SDL_Color Colour, SDL_Renderer* text_render, int x, int y, string text
 bool checkCollision(SDL_Rect a, SDL_Rect b)
 {
 	//Sides of the rectangles 
-	int leftA, leftB; 
-	int rightA, rightB; 
-	int topA, topB; 
+	int leftA, leftB;
+	int rightA, rightB;
+	int topA, topB;
 	int bottomA, bottomB;
 
 	//Calculating the sides of Rectangle A (Which will most likely be the player)
-	leftA = a.x; 
-	rightA = a.x + a.w; 
-	topA = a.y; 
-	bottomA = a.y + a.h; 
+	leftA = a.x;
+	rightA = a.x + a.w;
+	topA = a.y;
+	bottomA = a.y + a.h;
 
 	//Calculating the sides of Rectangle B (Which will most likely be the obstacle)
-	leftB = b.x; 
-	rightB = b.x + b.w; 
-	topB = b.y; 
+	leftB = b.x;
+	rightB = b.x + b.w;
+	topB = b.y;
 	bottomB = b.y + b.h;
 
 	//Checking to see if there has been a collision. 
@@ -649,7 +617,7 @@ bool checkCollision(SDL_Rect a, SDL_Rect b)
 	}
 	if (topA > +bottomB)
 	{
-		return false; 
+		return false;
 	}
 	if (rightA <= leftB)
 	{
@@ -661,7 +629,7 @@ bool checkCollision(SDL_Rect a, SDL_Rect b)
 	}
 
 	//If there has been a collision 
-	return true; 
+	return true;
 
 
 }
@@ -683,8 +651,8 @@ void compare(int score)
 	int hs = stoi(highscore);
 	if (score > hs)
 	{
-		hs = score; 
-		MyFile << hs; 
+		hs = score;
+		MyFile << hs;
 
 	}
 
--- Project1/Obstacle.cpp
+++ Project1/Obstacle.cpp
@@ -8,25 +8,21 @@ using namespace std;
 int obstacle::obstacle_x(int last_x)
 {
 	//X Coordinate
-    //Random X Coordinate between something and 1250
+    //Created from the last used x corrdinate
 	int obstacle_x = 0;
-	//srand((unsigned)time(NULL));
-	//obstacle_x = 750 + (rand() % 500);
 	obstacle_x = (last_x / 1.05); //last_x - 100
-	cout << "The obstacle x is" << obstacle_x << endl;
 	return obstacle_x;
 }
 
 int obstacle::obstacle_y(int type)
 {
-	//Y Coordinate, Width and Height Sorting Out
+	//Y Coordinate creation
 	int obstacle_y = 0;
 	if (type == 1)
 	{
 		//Ground Obstacle Code
 		//Needs to be on the height of the ground which is 200
 		obstacle_y = 550;
-		cout << "Obstacle y is" << obstacle_y << endl;
 		return obstacle_y;
 	}
 
@@ -35,7 +31,6 @@ int obstacle::obstacle_y(int type)
 		//Flying Obstacle Code
 		//Needs to be above the ground, so a greater y value. 
 		obstacle_y = 450;
-		cout << "Obstacle y is" << obstacle_y << endl;
 		return obstacle_y;
 	}
 
@@ -44,7 +39,7 @@ int obstacle::obstacle_y(int type)
 
 int obstacle::obstacle_width(int type)
 {
-	//Y Coordinate, Width and Height Sorting Out
+	//Width Creation
 	int obstacle_width = 0;
 	if (type == 1)
 	{
@@ -65,87 +60,20 @@ int obstacle::obstacle_width(int type)
 
 int obstacle::obstacle_height(int type)
 {
-	//Y Coordinate, Width and Height Sorting Out
+	//Height Creation
 	int obstacle_height = 0;
 	if (type == 1)
 	{
 		//Ground Obstacle Code
-		obstacle_height = 100;
-		cout << "Obstacle Height is" << obstacle_height << endl; 
+		obstacle_height = 150;
 		return obstacle_height;
 	}
 
 	else if (type == 2)
 	{
 		//Flying Obstacle Code
-		obstacle_height = 50;
-		cout << "Obstacle Height is" << obstacle_height << endl;
+		obstacle_height = 125;
 		return obstacle_height;
 	}
 }
 
-//void obstacle::create(int type)
-//{
-//	//X Coordinate
-//	//Random X Coordinate between something and 1250
-//	cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-//	srand((unsigned)time(NULL));
-//	obstacle_x = 750 + (rand() % 500);
-//
-//	//Y Coordinate, Width and Height Sorting Out
-//	if (type == 1)
-//	{
-//		//Ground Obstacle Code
-//		//Needs to be on the height of the ground which is 200
-//		obstacle_y = 200;
-//		obstacle_height = 100;
-//		obstacle_width = 25;
-//	}
-//
-//	else if (type == 2)
-//	{
-//		//Flying Obstacle Code
-//		//Needs to be above the ground, so a greater y value. 
-//		obstacle_y = 350;
-//		obstacle_height = 50;
-//		obstacle_width = 100;
-//	}
-//}
-//
-//void obstacle::draw(int type, SDL_Renderer* render)
-//{
-//	cout << "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
-//	SDL_Rect Obs;
-//	Obs.x = obstacle_x;
-//	Obs.y = obstacle_y;
-//	Obs.w = obstacle_width;
-//	Obs.h = obstacle_height;
-//
-//	//Drawing it on screen
-//	if (type == 1)
-//	{
-//		//Ground Obstacle
-//		SDL_Surface* groundimage = SDL_LoadBMP("cactus.jpg");
-//		SDL_Texture* groundtexture = SDL_CreateTextureFromSurface(render, groundimage);
-//		SDL_RenderCopy(render, groundtexture, NULL, &Obs);
-//		SDL_RenderPresent(render);
-//	}
-//
-//	else if (type == 2)
-//	{
-//		//Flying Obstacle
-//		SDL_Surface* flyingimage = SDL_LoadBMP("bird.jpg");
-//		SDL_Texture* flyingtexture = SDL_CreateTextureFromSurface(render, flyingimage);
-//		SDL_RenderCopy(render, flyingtexture, NULL, &Obs);
-//		SDL_RenderPresent(render);
-//	}
-//
-//}
-//
-//void obstacle::move()
-//{
-//	while (obstacle_x != -5)
-//	{
-//		obstacle_x -= 1; 
-//	}
-//}
